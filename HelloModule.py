8a131413130fdb4177cb5fd475577263bfdbc8ac
Maz99992003 <mazen20039999@gmail.com>
2022-09-28 16:39:37 -0400

    turtle game

---
+++ HelloModule.py
@@ -0,0 +1,9 @@
+class HelloMessage:
+    def show(self):
+        helloMsg = 'Hello programming world'
+        print(helloMsg)
+        courseMsg = 'Programming Principles with Python'
+        print(courseMsg)
+        courseNo = 10004
+        print('#prog', courseNo, sep="") 
+    
